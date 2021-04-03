a01edd6ad1b85d24127d22c547e22387b482e0d8
pablo-cor <pablob@projectcor.com>
2021-04-03 00:18:26 -0300

    clase formularios

--- m8-js-front/c2-eventos/live-coding/scripts/main.js
+++ m8-js-front/c2-eventos/live-coding/scripts/main.js
@@ -1,7 +1,41 @@
 
-window.addEventListener("load", function() {
-    document.querySelector("#button").addEventListener("click", function(e) {
-        console.log(e.target)
-        console.log(this)
-    })
+const button = document.querySelector(".button-accept")
+
+/* button.onclick = function() {
+
+} */
+
+
+
+button.addEventListener("click", function() {
+    console.log('click en el boton', this)
+    
+})
+
+
+button.addEventListener("mouseover", function() {
+    /* console.log('mouseover en el boton') */
+})
+
+button.addEventListener("mouseout", function() {
+    /* console.log('mouseout en el boton') */
+})
+
+button.addEventListener("mousemove", function(event) {
+    /* console.log('mousemove en el boton') */
+})
+
+button.addEventListener("dblclick", function(event) {
+    /* console.log('dblclick en el boton') */
+})
+
+
+
+let acumulator = ""
+
+window.addEventListener("keydown", function(event) {
+    console.log(event)
+    const key = event.key
+    acumulator += key
+    
 })
---
+++ m8-js-front/c3-formularios/README.md
@@ -0,0 +1,20 @@
+# JS DOM / Formularios
+
+## Eventos
+
+- form.addEventListener("submit)
+- button.addEventListener("click)
+- field.value
+- e.preventDefault()
+- input.addEventListener("focus)
+- input.addEventListener("blur)
+- input.addEventListener("change)
+
+## Manejo de errores
+
+    1. Mensaje global
+    2. Listado de errores por campos
+    3. Señalización en el campo
+    4. Chequeo en focus
+    5. Íconos / colores en los campos
+
